bae6ecd7315681dd27083b497d1a5757597227c7
John Kitchin <jkitchin@andrew.cmu.edu>
2015-12-13 15:24:38 -0500

    modified name of file. not sure why it was doi.el before.

--- org-ref-test.el
+++ org-ref-test.el
@@ -1,5 +1,5 @@
 (when (require 'undercover nil t)
-  (undercover "doi.el" (:exclude "*-test.el")))
+  (undercover "org-ref.el" (:exclude "*-test.el")))
 
 (ert-deftest plain-and-simple ()
   (should t))
