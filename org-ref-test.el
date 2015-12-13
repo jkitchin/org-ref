b3a8fa4a2d718519ac3f4e32e3d46773e7c8a824
John Kitchin <jkitchin@andrew.cmu.edu>
2015-12-13 16:38:47 -0500

    minor change to remove a byte-compile warning.

--- org-ref-test.el
+++ org-ref-test.el
@@ -1,5 +1,5 @@
-(when (require 'undercover nil t)
-  (undercover "org-ref.el" (:exclude "*-test.el")))
+(require 'undercover nil t)
+(undercover "org-ref.el" (:exclude "*-test.el"))
 
 (ert-deftest plain-and-simple ()
   (should t))
