7f949ece8cdb56bd56e2f8dcfb5aa26a8818a205
John Kitchin <jkitchin@andrew.cmu.edu>
2015-12-13 19:09:37 -0500

    minor fix in link

--- arxiv.el
+++ arxiv.el
@@ -40,12 +40,14 @@
 (org-add-link-type
  "arxiv"
  ;; clicking
- (lambda (link-string) (browse-url (format "http://arxiv.org/abs/%s" link-string)))
+ (lambda (link-string)
+   (browse-url (format "http://arxiv.org/abs/%s" link-string)))
  ;; formatting
  (lambda (keyword desc format)
    (cond
     ((eq format 'html)
-     (format  "<a href=\"http://arxiv.org/abs/%s\">arxiv:%s</a>" keyword  keyword))
+     (format  "<a href=\"http://arxiv.org/abs/%s\">arxiv:%s</a>"
+	      keyword  (or desc keyword)))
     ((eq format 'latex)
      ;; write out the latex command
      (format "\\url{http://arxiv.org/abs/%s}{%s}" keyword (or desc keyword))))))
