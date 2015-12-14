83c911d9aa76ad9442aca3ec5b9d085f164e3f43
John Kitchin <jkitchin@andrew.cmu.edu>
2015-12-13 19:00:42 -0500

    rm api key

--- worldcat.el
+++ worldcat.el
@@ -1,21 +1,13 @@
+;;; worldcat.el --- Worldcat library
 
+
+;;; Commentary:
+;; See:
 ;; https://www.worldcat.org/wcpa/content/affiliate/default.jsp
-
-
 ;; https://www.worldcat.org/affiliate/tools?atype=text
 
-;;Key Z65ynvQVhbmmxq07PX3IN1n71Z91pjUXYSSCCP9p2n8gD241P46jMyMHDuAvqRejIX1g8YbrwOAbobBM
-;;Secret XAWkEvC+kOKPpGgtBVwmFQ==
-;; Status ACTIVE
-;; Environment Sandbox
-;; Expiration Date 06/27/2016
-;; Registry ID 128807
-;; Override Institutions
-;; Redirect URI
-;; Services
-;; WorldCat Search API (wcapi)
-;; View API Documentation
-;; Sandbox Info: Requests limited to 100/day
+
+;;; Code:
 
 (defun worldcat-query-all (query)
   "Open browser to Worldcat QUERY."
@@ -23,3 +15,7 @@
    (format
     "http://www.worldcat.org/search?qt=worldcat_org_all&q=%s"
     query)))
+
+(provide 'worldcat)
+
+;;; worldcat.el ends here
