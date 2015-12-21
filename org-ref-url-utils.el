61a7263af48f4281a23c4ab6518a435cf47d36c2
John Kitchin <jkitchin@andrew.cmu.edu>
2015-12-21 09:09:44 -0500

    separate out the url scrapers
    This should make it possible to add recipes without modifying the code.

--- org-ref-url-utils.el
+++ org-ref-url-utils.el
@@ -27,112 +27,118 @@
 ;;; Code:
 (require 'doi-utils)
 
+(defvar org-ref-url-scrapers
+  '( ;; ACS journals
+    ((string-match "http://pubs.acs.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "scheme=\"doi\" content=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; AIP journals
+    ((string-match "http://scitation.aip.org/" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "citation_doi\" content=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; APS journals
+    ((string-match "http://journals.aps.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "data-doi=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; http://ecst.ecsdl.org ECS
+    ((string-match "http://ecst.ecsdl.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
+       (match-string 1)))
+
+    ;; http://iopscience.iop.org
+    ((string-match "http://iopscience.iop.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "name=\"citation_doi\" content=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; http://jes.ecsdl.org
+    ((string-match "http://jes.ecsdl.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
+       (match-string 1)))
+
+    ;; http://www.jstor.org
+    ((string-match "http://www.jstor.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "objectDOI\" : \"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; <meta name="citation_doi" content="doi:10.1038/nature15540">
+    ((string-match "http://www.nature.com" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "name=\"citation_doi\" content=\"doi:\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; http://www.pnas.org
+    ((string-match "http://www.pnas.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
+       (match-string 1)))
+
+    ;; http://pubs.rsc.org
+    ((string-match "http://pubs.rsc.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "scheme=\"doi\" content=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; Science Magazine
+    ((string-match "http://www.sciencemag.org" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
+       (match-string 1)))
+
+    ;; ScienceDirect
+    ((string-match "http://www.sciencedirect.com/science/article" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "doi = '\\([^']*\\)'")
+       (match-string 1)))
+
+    ;; Springer journals
+    ((string-match "http://link.springer.com" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "name=\"citation_doi\" content=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; http://www.tandfonline.com Taylor and Francis
+    ((string-match "http://www.tandfonline.com" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "scheme=\"doi\" content=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; Wiley journals
+    ((string-match "http://onlinelibrary.wiley.com" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "name=\"citation_doi\" content=\"\\([^\"]*\\)\"")
+       (match-string 1)))
+
+    ;; A Pubmed page
+    ((string-match "http://www.ncbi.nlm.nih.gov/pubmed" url)
+     (with-current-buffer (url-retrieve-synchronously url)
+       (re-search-forward "\"http://dx.doi.org/\\([^\"]*\\)\"")
+       (match-string 1))))
+  "cond blocks to match urls, and a recipe to extract a DOI. This
+  variable exists to make it easy for users to add new recipes.")
+
 (defun org-ref-scrape-doi (url)
   "Scrape a doi from a URL.
 These all work by regular expressions that were
-reverse-engineered for each publisher."
-  (cond
-   ;; ACS journals
-   ((string-match "http://pubs.acs.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "scheme=\"doi\" content=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; AIP journals
-   ((string-match "http://scitation.aip.org/" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "citation_doi\" content=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; APS journals
-   ((string-match "http://journals.aps.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "data-doi=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; http://ecst.ecsdl.org ECS
-   ((string-match "http://ecst.ecsdl.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
-      (match-string 1)))
-
-   ;; http://iopscience.iop.org
-   ((string-match "http://iopscience.iop.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "name=\"citation_doi\" content=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; http://jes.ecsdl.org
-   ((string-match "http://jes.ecsdl.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
-      (match-string 1)))
-
-   ;; http://www.jstor.org
-   ((string-match "http://www.jstor.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "objectDOI\" : \"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; <meta name="citation_doi" content="doi:10.1038/nature15540">
-   ((string-match "http://www.nature.com" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "name=\"citation_doi\" content=\"doi:\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; http://www.pnas.org
-   ((string-match "http://www.pnas.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
-      (match-string 1)))
-
-   ;; http://pubs.rsc.org
-   ((string-match "http://pubs.rsc.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "scheme=\"doi\" content=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; Science Magazine
-   ((string-match "http://www.sciencemag.org" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "content=\"\\([^\"]*\\)\" name=\"citation_doi")
-      (match-string 1)))
-
-   ;; ScienceDirect
-   ((string-match "http://www.sciencedirect.com/science/article" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "doi = '\\([^']*\\)'")
-      (match-string 1)))
-
-   ;; Springer journals
-   ((string-match "http://link.springer.com" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "name=\"citation_doi\" content=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; http://www.tandfonline.com Taylor and Francis
-   ((string-match "http://www.tandfonline.com" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "scheme=\"doi\" content=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; Wiley journals
-   ((string-match "http://onlinelibrary.wiley.com" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "name=\"citation_doi\" content=\"\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   ;; A Pubmed page
-   ((string-match "http://www.ncbi.nlm.nih.gov/pubmed" url)
-    (with-current-buffer (url-retrieve-synchronously url)
-      (re-search-forward "\"http://dx.doi.org/\\([^\"]*\\)\"")
-      (match-string 1)))
-
-   (t
-    (message
-     "We don't know how to get a doi from %s. Try adding the doi by hand."
-     url)
-    nil)))
+reverse-engineered for each publisher. You can add new scraper
+recipes by adding to `org-ref-url-scrapers'. A recipe looks like:
+((conditional statement matching a url) expressions that return a doi.)"
+  (eval `(cond
+	  ,@org-ref-url-scrapers
+	  (t
+	   (message
+	    "We don't know how to get a doi from %s. Try adding the doi by hand."
+	    url)
+	   nil))))
 
 
 (defun org-ref-url-dnd-doi-func (event)
