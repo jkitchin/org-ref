cf3504a8fafd9e0ebf625227d43fab7b122c928e
John Kitchin <jkitchin@andrew.cmu.edu>
2016-02-18 14:44:51 -0500

    new export tests

--- org-ref-test.el
+++ org-ref-test.el
@@ -14,6 +14,8 @@
 (require 'org-ref)
 (load-file "tests/org-test.el")
 
+
+
 (when (require 'undercover nil t)
   (undercover "org-ref.el" (:exclude "*-test.el")))
 
@@ -213,7 +215,7 @@ bibliography:tests/test-1.bib
 	(org-ref-find-bibliography))))))
 
 
-;;* bibtex tests We rely alot on bibtex functionality. These are tests to make
+;;* bibtex tests We rely a lot on bibtex functionality. These are tests to make
 ;; sure it works as we expect. I don't have clear evidence, but I feel like I
 ;; have had trouble with the in the past.
 (ert-deftest bib-1 ()
@@ -525,6 +527,87 @@ file:not.here  [[./or.here]] and not attachfile:or.anywhere"
 bibliography:tests/test-1.bib
 "
 	      (org-ref-sort-citation-link)))))
+
+
+
+;;* exports
+(ert-deftest cite-export-1 ()
+  (should
+   (string=
+    "\\cite{kitchin-2008-alloy}
+"
+    (org-test-with-temp-text
+	"cite:kitchin-2008-alloy"
+      (org-latex-export-as-latex nil nil nil t)
+      (buffer-substring-no-properties (point-min) (point-max))))))
+
+(ert-deftest cite-export-2 ()
+  (should
+   (string=
+    "\\cite[page 2]{kitchin-2008-alloy}
+"
+    (org-test-with-temp-text
+	"[[cite:kitchin-2008-alloy][page 2]]"
+      (org-latex-export-as-latex nil nil nil t)
+      (buffer-substring-no-properties (point-min) (point-max))))))
+
+(ert-deftest cite-export-3 ()
+  (should
+   (string=
+    "\\cite[page 2][post text]{kitchin-2008-alloy}
+"
+    (org-test-with-temp-text
+	"[[cite:kitchin-2008-alloy][page 2::post text]]"
+      (org-latex-export-as-latex nil nil nil t)
+      (buffer-substring-no-properties (point-min) (point-max))))))
+
+(ert-deftest label-export-1 ()
+  (should
+   (string=
+    "\\label{test}
+"
+    (org-test-with-temp-text
+	"label:test"
+      (org-latex-export-as-latex nil nil nil t)
+      (buffer-substring-no-properties (point-min) (point-max))))))
+
+(ert-deftest ref-export-1 ()
+  (should
+   (string=
+    "\\ref{test}
+"
+    (org-test-with-temp-text
+	"ref:test"
+      (org-latex-export-as-latex nil nil nil t)
+      (buffer-substring-no-properties (point-min) (point-max))))))
+
+
+(ert-deftest bib-export-1 ()
+  (should
+   (string=
+    (format
+     "\\bibliography{%s}
+" (file-relative-name "test"))
+    (org-test-with-temp-text
+	"bibliography:test.bib"
+      (org-latex-export-as-latex nil nil nil t)
+      (buffer-substring-no-properties (point-min) (point-max))))))
+
+
+(ert-deftest bib-export-1 ()
+  (should
+   (string=
+    (format
+     "\\bibliography{%s,%s}
+" (file-relative-name "test")
+(file-relative-name "titles"))
+(org-test-with-temp-text
+    "bibliography:test.bib,titles.bib"
+  (org-latex-export-as-latex nil nil nil t)
+  (buffer-substring-no-properties (point-min) (point-max))))))
+
+
+
 
 (provide 'org-ref-test)
 
