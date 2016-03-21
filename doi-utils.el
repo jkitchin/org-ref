7cd5301ad211aa5a1e4ab43d1b86cca9fe481010
Jonathan <jgrg@autistici.org>
2016-03-21 16:12:34 +0000

    Add only one line between entries
    I was getting an extra line every time the bibtex entry was inserted. I
    think it has to do with `require-final-newline` being set to t.

--- doi-utils.el
+++ doi-utils.el
@@ -732,7 +732,9 @@ Argument BIBFILE the bibliography to use."
       (if (search-forward doi nil t)
           (message "%s is already in this file" doi)
         (goto-char (point-max))
-        (insert "\n\n")
+	(if require-final-newline
+	    (insert "\n")
+	  (insert "\n\n"))
         (doi-utils-insert-bibtex-entry-from-doi doi)
         (save-buffer)))))
 
