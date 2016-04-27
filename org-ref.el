af0dac5e863746a34360d8c70f83103778409c92
John Kitchin <jkitchin@andrew.cmu.edu>
2016-04-27 15:30:37 -0400

    version bump

--- org-ref.el
+++ org-ref.el
@@ -4,7 +4,7 @@
 
 ;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
 ;; URL: https://github.com/jkitchin/org-ref
-;; Version: 0.8.0
+;; Version: 0.8.1
 ;; Keywords: org-mode, cite, ref, label
 ;; Package-Requires: ((dash "2.11.0") (helm "1.5.5") (helm-bibtex "2.0.0") (hydra "0.13.2") (key-chord "0") (s "1.10.0") (f "0.18.0") (emacs "24.4"))
 
