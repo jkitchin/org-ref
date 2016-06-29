9552e74b9a3dc03a9e8730608882be2feee99608
John Kitchin <jkitchin@andrew.cmu.edu>
2016-06-28 22:11:33 -0400

    rm org-ref-core req.
    it doesn't exist yet.

--- org-ref.el
+++ org-ref.el
@@ -6,8 +6,8 @@
 ;; URL: https://github.com/jkitchin/org-ref
 ;; Version: 0.8.1
 ;; Keywords: org-mode, cite, ref, label
-;; Package-Requires: ((dash "2.11.0") (helm "1.5.5") (helm-bibtex "2.0.0") (ivy "0.8.0") (hydra "0.13.2") (key-chord "0") (s "1.10.0") (f "0.18.0") (org-ref-core "0") (emacs "24.4"))
-
+;; Package-Requires: ((dash "2.11.0") (helm "1.5.5") (helm-bibtex "2.0.0") (ivy "0.8.0") (hydra "0.13.2") (key-chord "0") (s "1.10.0") (f "0.18.0")  (emacs "24.4"))
+;; (org-ref-core "0")
 ;; This file is not currently part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or
