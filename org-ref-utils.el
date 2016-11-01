bcc9e04d410534901844bbedb43c50eb37d7532b
Lixin Chin <lixin.chin@research.uwa.edu.au>
2016-11-01 16:25:02 +0800

    Bugfix: `org-ref-open-url-at-point' fallback to doi if url field doesn't exist.
    `bibtex-autokey-get-field' returns the empty string "" for fields that
    don't exist, not `nil'.

--- org-ref-utils.el
+++ org-ref-utils.el
@@ -387,16 +387,16 @@ Argument KEY is the bibtex key."
         ;; I like this better than bibtex-url which does not always find
         ;; the urls
         (catch 'done
-          (let ((url (bibtex-autokey-get-field "url")))
-            (when  url
-              (browse-url (s-trim url))
+          (let ((url (s-trim (bibtex-autokey-get-field "url"))))
+            (unless (s-blank? url)
+              (browse-url url)
               (throw 'done nil)))
 
-          (let ((doi (bibtex-autokey-get-field "doi")))
-            (when doi
+          (let ((doi (s-trim (bibtex-autokey-get-field "doi"))))
+            (unless (s-blank? doi)
               (if (string-match "^http" doi)
                   (browse-url doi)
-                (browse-url (format "http://dx.doi.org/%s" (s-trim doi))))
+                (browse-url (format "http://dx.doi.org/%s" doi)))
               (throw 'done nil))))))))
 
 
