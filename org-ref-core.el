7adf252f4cee3a4baaf08fcea7803b50e98adf2a
Win Treese <treese@acm.org>
2017-10-08 22:56:50 -0400

    Fix typo in code change.

--- org-ref-core.el
+++ org-ref-core.el
@@ -2758,7 +2758,7 @@ file.  Makes a new buffer with clickable links."
          (other-fields)
          (type (cdr (assoc "=type=" entry)))
          (key (cdr (assoc "=key=" entry)))
-	 (field-order (cdr (assoc (if type (downcase-type))
+	 (field-order (cdr (assoc (if type (downcase type))
 				  org-ref-bibtex-sort-order))))
 
     ;; these are the fields we want to order that are in this entry
