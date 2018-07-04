0419865f65e9c9165c565ef8e0e5d8ffafcd6613
John Kitchin <jkitchin@andrew.cmu.edu>
2018-07-04 08:56:36 -0700

    Fix issue reported in https://github.com/tmalsburg/helm-bibtex/issues/250.
    I guess the issue is the temp-buffer was not in bibtex-mode, which prevented it
    from working.

--- org-ref-helm-bibtex.el
+++ org-ref-helm-bibtex.el
@@ -122,6 +122,7 @@ The cdr of the the cons cell is the function to use."
 Used as a new action in `helm-bibtex'.
 CANDIDATE is ignored."
   (with-temp-buffer
+    (bibtex-mode)
     (mapc #'insert-file-contents
 	  (-flatten (list bibtex-completion-bibliography)))
 
@@ -133,8 +134,11 @@ CANDIDATE is ignored."
 					  "\\)[[:space:]]*[\(\{][[:space:]]*"
 					  (regexp-quote bibtex-key)
 					  "[[:space:]]*,"))
-	       (bibtex-mark-entry)
-	       (cl-pushnew  (buffer-substring (point) (mark)) entries))
+
+	       (cl-pushnew  (buffer-substring
+			     (bibtex-beginning-of-entry)
+			     (bibtex-end-of-entry))
+			    entries))
 
       (with-temp-buffer
 	(dolist (entry entries)
