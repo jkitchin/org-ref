b09ef032362a57d8a6339a26795c5f80e2c0c297
John Kitchin <jkitchin@andrew.cmu.edu>
2018-10-10 19:43:19 -0400

    Try to add org-ref-pkg.el in response to issue #562.

---
+++ org-ref-pgk.el
@@ -0,0 +1,16 @@
+(define-package "org-ref" "1.1.1" "citations, cross-references and bibliographies in org-mode"
+  '((dash "2.11.0")
+    (htmlize "1.51")
+    (helm "1.5.5")
+    (helm-bibtex "2.0.0")
+    (ivy "0.8.0")
+    (hydra "0.13.2")
+    (key-chord "0")
+    (s "1.10.0")
+    (f "0.18.0")
+    (emacs "24.4")
+    (pdf-tools "0.7")))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
