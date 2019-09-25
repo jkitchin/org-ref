jkitchin/org-ref

commit b12b8f05c323e5a50aaf82d6aedd1c8be1ac34ba

leave a blank line between entries

doi-utils.el
@@ -768,8 +768,9 @@ Argument BIBFILE the bibliography to use."
       (if (search-forward doi nil t)
           (message "%s is already in this file" doi)
         (goto-char (point-max))
-	(if require-final-newline
-	    (insert "\n")
+	(if (re-search-backward "^}$" nil t)
+	    (progn (forward-char 1)
+		   (insert "\n\n"))
 	  (insert "\n\n"))
         (doi-utils-insert-bibtex-entry-from-doi doi)
         (save-buffer)))))

commit ba70352b4e4361f1c79f4bc1f2ab94bfd9ada0df

Fix doi-utils-add-bibtex-entry-from-doi when no trailing newline
doi-utils-add-bibtex-entry-from-doi will fail to add an bibtex entry if the bib
file does not have ending newline, and report end of buffer. There is no point
in running (forward-char 1) after (goto-char (point-max)), since you are already
at the end of the document. The bug was introduced in commit b12b8f05.

doi-utils.el
@@ -981,9 +981,6 @@ Argument BIBFILE the bibliography to use."
       (if (word-search-forward (concat doi) nil t)
           (message "%s is already in this file" doi)
         (goto-char (point-max))
-	;; make sure we are at the beginning of a line
-	(when (not (= (point) (line-beginning-position)))
-	  (forward-char 1))
 
 	(when (not (looking-back "\n\n" (min 3 (point))))
 	  (insert "\n\n"))