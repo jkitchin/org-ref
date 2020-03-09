e3eb9215a540ba62a0b0253d003c704b7740deeb
John Kitchin <johnrkitchin@gmail.com>
2020-03-09 08:31:40 -0400

    update versions on some required packages.
    This is mentioned in issue #711

--- org-ref.el
+++ org-ref.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/jkitchin/org-ref
 ;; Version: 1.1.1
 ;; Keywords: org-mode, cite, ref, label
-;; Package-Requires: ((dash "2.11.0") (htmlize "1.51") (helm "1.5.5") (helm-bibtex "2.0.0") (ivy "0.8.0") (hydra "0.13.2") (key-chord "0") (s "1.10.0") (f "0.18.0")  (emacs "24.4") (pdf-tools "0.7"))
+;; Package-Requires: ((dash "2.17.0") (htmlize "1.51") (helm "1.5.5") (helm-bibtex "2.0.0") (ivy "0.13.0") (hydra "0.15") (key-chord "0") (s "1.12.0") (f "0.20.0")  (emacs "25.1") (pdf-tools "0.9"))
 ;; This file is not currently part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or
