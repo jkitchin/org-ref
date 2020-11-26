120509c38929cc25d814e9a42092c44cb34ec34e
John Kitchin <jkitchin@andrew.cmu.edu>
2020-11-26 14:24:56 -0500

    Comment out the defalias for dnd--unescape-uri
    I added this in issue #812 , but it appears these functions are not autoloaded
    and so this creates a warning that is not helpful. For now I am commenting this
    out to avoid that. Later we will have to figure out a solution.

--- org-ref-pdf.el
+++ org-ref-pdf.el
@@ -42,10 +42,10 @@
 
 ;; See https://github.com/jkitchin/org-ref/issues/812
 ;; apparently there is a function name change coming in
-(if (and (not (fboundp 'dnd-unescape-uri))
-	 (fboundp 'dnd--escape-uri))
-    (defalias 'dnd-unescape-uri 'dnd--unescape-uri)
-  (warn "dnd-unescape-uri is undefined. Some things may not work."))
+;; (if (and (not (fboundp 'dnd-unescape-uri))
+;; 	 (fboundp 'dnd--escape-uri))
+;;     (defalias 'dnd-unescape-uri 'dnd--unescape-uri)
+;;   (warn "dnd-unescape-uri is undefined. Some things may not work."))
 
 (defgroup org-ref-pdf nil
   "Customization group for org-ref-pdf"
