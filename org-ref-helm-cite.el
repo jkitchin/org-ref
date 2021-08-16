a494794c0c52e1d05e8e13712c5512c08c26f189
John Kitchin <jkitchin@andrew.cmu.edu>
2021-08-16 17:19:23 -0400

    probable fix for issue #490

--- org-ref-helm-cite.el
+++ org-ref-helm-cite.el
@@ -346,10 +346,12 @@ change the key at point to the selected keys."
 	  ;; goto next comma or end
 	  (re-search-forward
 	   ","
-	   (org-element-property :end object) t)
+	   (org-element-property :end object) 'mv)
 	  (skip-chars-backward " ")
+	  (unless (looking-back "," 1) (insert ","))
 	  (insert (mapconcat 'identity keys ","))
-	  (unless (looking-at ",") (insert ",")))))
+	  ;; (unless (looking-at " ") (insert ","))
+	  )))
        ;; double prefix, replace key at point
        ((equal helm-current-prefix-arg '(16))
         (setf (buffer-substring
@@ -579,7 +581,7 @@ Checks for pdf and doi, and add appropriate functions."
     	    '("Open pdf" . (lambda ()
     			     (funcall org-ref-open-pdf-function)))
     	    candidates))
-	  
+
 	  ;; try with doi
     	  (t
     	   (cl-pushnew
