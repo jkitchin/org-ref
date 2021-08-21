fcd7d5e9f517805404a054b371a89dc1944648fa
John Kitchin <jkitchin@andrew.cmu.edu>
2021-08-21 11:27:21 -0400

    initial commit of export for non-latex backends

---
+++ org-ref-export.el
@@ -0,0 +1,277 @@
+;;; org-ref-export.el --- org-ref-export library
+
+;;; Commentary:
+;; This is an export library that uses CSL to process the citations and bibliography.
+;;
+;; It is intended for non-LaTeX exports, but you can also use it with LaTeX if
+;; you want to avoid using bibtex/biblatex for some reason.
+;;
+;; Note that citeproc does not do anything for cross-references, so if non-latex
+;; export is your goal, you should be careful in how you do cross-references,
+;; and rely exclusively on org-syntax for that, e.g. radio targets and fuzzy
+;; links.
+;;
+;; TODO: write refproc to take care of cross-references.
+
+;;; Code:
+
+(require 'citeproc)
+
+
+(defun org-ref-process-buffer (csl-backend)
+  "Process the citations and bibliography in the org-buffer.
+Usually run on a copy of the buffer during export.
+CSL-BACKEND is the backend that citeproc uses."
+
+  (let* ((style (or (cadr (assoc "CSL-STYLE"
+				 (org-collect-keywords
+				  '("CSL-STYLE"))))
+		    "apa-numeric-superscript-brackets.csl"))
+	 (locale (or (cadr (assoc "CSL-LOCALE"
+				  (org-collect-keywords
+				   '("CSL-LOCALE"))))
+		     "en-US"))
+
+	 (proc (citeproc-create (if (file-exists-p style)
+				    style
+				  (f-join org-cite-csl-styles-dir style))
+				(citeproc-itemgetter-from-bibtex (org-ref-find-bibliography))
+				(citeproc-locale-getter-from-dir org-cite-csl-locales-dir)
+				locale))
+
+	 ;; list of links in the buffer
+	 (cite-links (org-element-map (org-element-parse-buffer) 'link
+		       (lambda (lnk)
+			 (when (member (org-element-property :type lnk) org-ref-cite-types)
+			   lnk))))
+
+	 (cites (cl-loop for cl in cite-links collect
+			 (let* ((cite-data (org-ref-parse-cite-path (org-element-property :path cl)))
+				;; TODO where does this go?
+				;; concat to first and last?
+				(common-prefix (or (plist-get cite-data :prefix) ""))
+				(common-suffix (or (plist-get cite-data :suffix) ""))
+				(refs (plist-get cite-data :references))
+				(cites (cl-loop for ref in refs collect
+						`((id . ,(plist-get ref :key))
+						  (prefix . ,(or (plist-get ref :prefix) ""))
+
+						  ;; I think the locator, label, location
+						  ;; are part of
+						  ;; the suffix,
+						  ;; but I need to
+						  ;; study how to
+						  ;; split it out.
+						  ;; for now I
+						  ;; leave it in
+						  ;; the suffix.
+						  (suffix . ,(or (plist-get ref :suffix) ""))
+						  (locator . nil)
+						  (label . nil)
+						  (location . nil)
+						  ;; TODO: proof of concept and not complete
+						  (suppress-author . ,(member
+								       (org-element-property :type cl)
+								       '("citenum"
+									 "citeyear"
+									 "citeyear*"
+									 "citedate"
+									 "citedate*"
+									 "citetitle"
+									 "citetitle*"
+									 "citeurl")))))))
+			   ;; To handle common prefixes, suffixes, I just concat them with the first/last entries.
+			   (setf (cdr (assoc 'prefix (cl-first cites)))
+				 (concat common-prefix (cdr (assoc 'prefix (cl-first cites)))))
+			   (setf (cdr (assoc 'suffix (car (last cites))))
+				 (concat (cdr (assoc 'suffix (car (last cites)))) common-suffix))
+
+
+			   ;; https://github.com/andras-simonyi/citeproc-el#creating-citation-structures
+			   (citeproc-citation-create :cites
+						     ;; I am not sure why this
+						     ;; should be reversed. The
+						     ;; order is backwards
+						     ;; without it though.
+						     (reverse cites)
+
+						     ;; TODO: proof of concept, incomplete
+						     ;; if this is true, the citation is not parenthetical
+						     :suppress-affixes (let ((type (org-element-property :type cl)))
+									 (when (member type '("citet"
+											      "citet*"
+											      "citenum"))
+									   t))
+
+						     ;; TODO: this is proof of
+						     ;; concept, and not
+						     ;; complete mode is one of
+						     ;; suppress-author,
+						     ;; textual, author-only,
+						     ;; year-only, or nil for
+						     ;; default
+						     :mode (let ((type (org-element-property :type cl)))
+							     (cond
+							      ((member type '("citet" "citet*"))
+							       'textual)
+							      ((member type '("citeyear" "citeyear*"))
+							       'year-only)
+							      ((member type '("citeauthor" "citeauthor*"))
+							       'author-only)
+							      ((member type '("citedate" "citedate*"))
+							       'suppress-author)
+							      (t
+							       nil)))
+
+						     ;; I think the capitalized styles are what this is for
+						     :capitalize-first (string-match
+									"[A-Z]"
+									(substring (org-element-property :type cl) 0 1))
+						     ;; I don't know where this information would come from
+						     :note-index nil
+						     :ignore-et-al nil))))
+
+	 (rendered-citations (progn (citeproc-append-citations cites proc)
+				    (citeproc-render-citations proc csl-backend)))
+	 (rendered-bib (car (citeproc-render-bib proc csl-backend)))
+	 ;; The idea is we will wrap each citation and the bibliography in
+	 ;; org-code so it exports appropriately.
+	 (cite-formatters '((html . "@@html:%s@@")
+			    (latex . "@@latex:%s@@")
+			    (org-odt . "@@odt:%s@@")))
+	 (bib-formatters '((html .  "\n#+BEGIN_EXPORT html\n%s\n#+END_EXPORT\n")
+			   (latex .  "\n#+BEGIN_EXPORT latex\n%s\n#+END_EXPORT\n")
+			   (org-odt . "\n#+BEGIN_EXPORT ODT\n%s\n#+END_EXPORT\n"))))
+
+    ;; replace the cite links
+    (cl-loop for cl in (reverse  cite-links) for rc in (reverse rendered-citations) do
+	     (cl--set-buffer-substring (org-element-property :begin cl)
+				       (org-element-property :end cl)
+				       (format (or (cdr (assoc backend cite-formatters)) "%s") rc)))
+
+    ;; replace the bibliography
+    (org-element-map (org-element-parse-buffer) 'link
+      (lambda (lnk)
+	(when (string= (org-element-property :type lnk) "bibliography")
+	  (cl--set-buffer-substring (org-element-property :begin lnk)
+				    (org-element-property :end lnk)
+				    (format (or (cdr (assoc backend bib-formatters)) "%s") rendered-bib)))))))
+
+
+
+(defun org-ref-export-to (backend &optional async subtreep visible-only
+				  body-only info)
+  "Export buffer to BACKEND.
+See `org-export-as' for the meaning of ASYNC SUBTREEP
+VISIBLE-ONLY BODY-ONLY and INFO."
+  (let* ((fname (buffer-file-name))
+	 (extensions '((html . ".html")
+		       (latex . ".tex")
+		       (md . ".md")
+		       (ascii . ".txt")
+		       (odt . ".odf")))
+	 (csl-formats '((html . html)
+			(latex . latex)
+			(md . plain)
+			(ascii . plain)
+			(odt . org-odt)))
+	 (export-name (concat (file-name-sans-extension fname)
+			      (or (cdr (assoc backend extensions)) ""))))
+
+    (org-export-with-buffer-copy
+     (org-ref-process-buffer (cdr (assoc backend csl-formats)))
+     (pcase backend
+       ;; odt is a little bit special
+       (odt
+	(org-open-file (org-odt-export-to-odt async subtreep visible-only
+					      info)
+		       'system))
+       (_)
+       (org-open-file (org-export-to-file backend export-name
+			async subtreep visible-only
+			body-only info)
+		      'system)))))
+
+
+(defun org-ref-export-to-html (&optional async subtreep visible-only
+					 body-only info)
+  "Export the buffer to HTML and open.
+See `org-export-as' for the meaning of ASYNC SUBTREEP
+VISIBLE-ONLY BODY-ONLY and INFO."
+  (org-ref-export-to 'html async subtreep visible-only
+		     body-only info))
+
+
+(defun org-ref-export-to-md (&optional async subtreep visible-only
+				       body-only info)
+  "Export the buffer to Markdown and open.
+See `org-export-as' for the meaning of ASYNC SUBTREEP
+VISIBLE-ONLY BODY-ONLY and INFO."
+  (org-ref-export-to 'md async subtreep visible-only
+		     body-only info))
+
+
+(defun org-ref-export-to-ascii (&optional async subtreep visible-only
+					  body-only info)
+  "Export the buffer to ascii and open.
+See `org-export-as' for the meaning of ASYNC SUBTREEP
+VISIBLE-ONLY BODY-ONLY and INFO."
+  (org-ref-export-to 'ascii async subtreep visible-only
+		     body-only info))
+
+
+(defun org-ref-export-to-latex (&optional async subtreep visible-only
+					  body-only info)
+  "Export the buffer to LaTeX and open.
+See `org-export-as' for the meaning of ASYNC SUBTREEP
+VISIBLE-ONLY BODY-ONLY and INFO."
+  (org-ref-export-to 'latex async subtreep visible-only
+		     body-only info))
+
+
+(defun org-ref-export-to-odt (&optional async subtreep visible-only
+					body-only info)
+  "Export the buffer to ODT and open.
+See `org-export-as' for the meaning of ASYNC SUBTREEP
+VISIBLE-ONLY BODY-ONLY and INFO."
+  (require 'htmlfontify)
+  (unless (boundp 'hfy-user-sheet-assoc) (setq  hfy-user-sheet-assoc nil))
+  (org-ref-export-to 'odt async subtreep visible-only
+		     body-only info))
+
+
+(defun org-ref-export-as-org (&optional async subtreep visible-only
+					body-only info)
+  "Export the buffer to an ORG buffer and open.
+We only make a buffer here to avoid overwriting the original file.
+See `org-export-as' for the meaning of ASYNC SUBTREEP
+VISIBLE-ONLY BODY-ONLY and INFO."
+  (let* ((export-buf "*org-ref ORG Export*")
+	 export)
+
+    (org-export-with-buffer-copy
+     (org-ref-process-buffer 'org)
+     (setq export (org-export-as 'org))
+     (with-current-buffer (get-buffer-create export-buf)
+       (erase-buffer)
+       (org-mode)
+       (insert export)))
+    (pop-to-buffer export-buf)))
+
+
+
+
+
+(org-export-define-derived-backend 'org-ref 'org
+  :menu-entry
+  '(?r "Org-ref export"
+       ((?a "to Ascii" org-ref-export-as-ascii)
+	(?h "to html" org-ref-export-to-html)
+	(?l "to LaTeX" org-ref-export-as-latex)
+	(?m "to Markdown" org-ref-export-as-md)
+	(?o "to ODT" org-ref-export-as-odt)
+	(?O "to Org buffer" org-ref-export-as-org))))
+
+(provide 'org-ref-export)
+
+;;; org-ref-export.el ends here
