53f336b4a047201b45fc416390d48a53a69535fa
John Kitchin <jkitchin@andrew.cmu.edu>
2021-08-25 14:01:54 -0400

    add insert functions, package requires

--- org-ref-ivy.el
+++ org-ref-ivy.el
@@ -6,6 +6,7 @@
 ;; URL: https://github.com/jkitchin/org-ref
 ;; Version: 1.0
 ;; Keywords: org-mode, cite, ref, label
+;; Package-Requires: ((org-ref "0") (ivy-bibtex "0"))
 
 ;; This file is not currently part of GNU Emacs.
 
@@ -25,17 +26,83 @@
 ;;; Commentary:
 
 (require 'org-ref-core)
-(require 'org-ref-bibliography-links)
-(require 'org-ref-citation-links)
-(require 'org-ref-ref-links)
-(require 'org-ref-label-link)
-(require 'org-ref-misc-links)
-(require 'org-ref-utils)
-(require 'org-ref-bibtex)
+(require 'ivy-bibtex)
+
+
+;; all these functions are defined in ivy-bibtex, but not in a variable. Here
+;; you can set them as you see fit.
+(defcustom org-ref-citation-alternate-insert-actions
+  '(("p" ivy-bibtex-open-pdf "Open PDF file (if present)")
+    ("u" ivy-bibtex-open-url-or-doi "Open URL or DOI in browser")
+    ;; this insert-citation only inserts an org-ref cite.
+    ;; ("c" ivy-bibtex-insert-citation "Insert citation")
+    ("R" ivy-bibtex-insert-reference "Insert formatted reference")
+    ("k" ivy-bibtex-insert-key "Insert BibTeX key")
+    ("b" ivy-bibtex-insert-bibtex "Insert BibTeX entry")
+    ("a" ivy-bibtex-add-PDF-attachment "Attach PDF to email")
+    ("e" ivy-bibtex-edit-notes "Edit notes")
+    ("s" ivy-bibtex-show-entry "Show entry")
+    ("l" ivy-bibtex-add-pdf-to-library "Add PDF to library")
+    ("f" (lambda (_candidate) (ivy-bibtex-fallback ivy-text)) "Fallback options"))
+  "Alternate actions to do instead of inserting.")
+
+
+(defun org-ref-cite-insert-ivy ()
+  "Function for inserting a citation."
+  (interactive)
+  (ivy-set-actions
+   'org-ref-cite-insert
+   org-ref-citation-alternate-insert-actions)
+
+  (ivy-set-display-transformer 'org-ref-cite-insert 'ivy-bibtex-display-transformer)
+
+  (bibtex-completion-init)
+  (let* ((bibtex-completion-bibliography (org-ref-find-bibliography))
+	 (candidates (bibtex-completion-candidates))
+	 (choice (ivy-read "BibTeX entries: " candidates
+			   :caller 'org-ref-cite-insert
+			   :multi-action (lambda (candidates)
+					   (org-ref-insert-cite-keys
+					    (cl-loop for candidate in candidates collect
+						     (cdr (assoc "=key=" (cdr candidate))))))
+			   :action '(1
+				     ("o" (lambda (candidate)
+					    (org-ref-insert-cite-key (cdr (assoc "=key=" (cdr candidate)))))
+				      "insert")
+				     ("r" (lambda (candidate)
+
+					    (let* ((object (org-element-context))
+						   (type (org-element-property :type object))
+						   (begin (org-element-property :begin object))
+						   (end (org-element-property :end object))
+						   (link-string (org-element-property :path object))
+						   (data (org-ref-parse-cite-path link-string))
+						   (references (plist-get data :references))
+						   (cp (point))
+						   (key)
+						   keys i)
+					      ;;   We only want this to work on citation links
+					      (when (-contains? org-ref-cite-types type)
+						(setq key (org-ref-get-bibtex-key-under-cursor))
+						(if (null key)
+						    ;; delete the whole cite
+						    (cl--set-buffer-substring begin end "")
+						  (setq i (seq-position references key
+									(lambda (el key)
+									  (string= key (plist-get el :key)))))
+						  (setf (plist-get (nth i references) :key)
+							(cdr (assoc "=key=" (cdr candidate))))
+						  (setq data (plist-put data :references references))
+						  (save-excursion
+						    (goto-char begin)
+						    (re-search-forward link-string)
+						    (replace-match (org-ref-interpret-cite-data data)))
+						  (goto-char cp)))))
+				      "Replace key at point")))))))
 
 
 (setq org-ref-insert-link-function 'org-ref-insert-link
-      org-ref-insert-cite-function 'org-ref-cite-insert
+      org-ref-insert-cite-function 'org-ref-cite-insert-ivy
       org-ref-insert-label-function nil
       org-ref-insert-ref-function 'org-ref-insert-ref-link
       org-ref-cite-onclick-function (lambda (_) (org-ref-citation-hydra/body)))
