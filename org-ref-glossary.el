27d97fd81dcc0007ebac4d3a2ea08a25c51eae77
lennonhill <31923882+lennonhill@users.noreply.github.com>
2021-09-30 15:12:47 +0200

    Fix variable binding

--- org-ref-glossary.el
+++ org-ref-glossary.el
@@ -487,18 +487,17 @@ This will run in `org-export-before-parsing-hook'."
     ("Acp"      . "Glspl")))
 
 
-(cl-loop for mapping in org-ref-glossary-acr-commands-mapping
-	 do
-	 (org-link-set-parameters (car mapping)
-				  :follow #'or-follow-acronym
-				  :face 'org-ref-acronym-face-fn
-				  :help-echo 'or-acronym-tooltip
-				  :export (lambda (path _ format)
-					    (cond
-					     ((memq format '(latex beamer))
-					      (format "\\%s{%s}" (cdr mapping) path))
-					     (t
-					      (format "%s" (upcase path)))))))
+(cl-dolist (mapping org-ref-glossary-acr-commands-mapping)
+  (org-link-set-parameters (car mapping)
+			   :follow #'or-follow-acronym
+			   :face 'org-ref-acronym-face-fn
+			   :help-echo 'or-acronym-tooltip
+			   :export (lambda (path _ format)
+				     (cond
+				      ((memq format '(latex beamer))
+				       (format "\\%s{%s}" (cdr mapping) path))
+				      (t
+				       (format "%s" (upcase path)))))))
 
 
 ;;** Tooltips on acronyms
