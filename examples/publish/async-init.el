2aa6dd6873a05435904b47a2be7ed02d70142728
Your Name <you@example.com>
2023-08-29 16:21:05 -0400

    first draft of org-ref-publish

---
+++ examples/publish/async-init.el
@@ -0,0 +1,4 @@
+;; You will have to modify this for your own system
+(load-file "/Users/jkitchin/Dropbox/emacs/scimax/init.el")
+(load-file "/Users/jkitchin/Dropbox/emacs/org-ref/org-ref-publish.el")
+
