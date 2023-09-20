a7049f51ab0a316908318f04641e73d8cca6f9e2
Justus Piater <justus.piater@uibk.ac.at>
2023-09-20 20:43:18 +0200

    org-ref-extract.el: org-ref--extract-entry-from-html: &rest more-fields
    Pass more-fields as separate args rather than as a list, to simplify
    the calling sites.

--- org-ref-extract.el
+++ org-ref-extract.el
@@ -54,7 +54,7 @@ FILE field to the entry."
 
 
 (defun org-ref--extract-entry-from-html
-    (html-buffer bibtex pdf-url &optional more-fields)
+    (html-buffer bibtex pdf-url &rest more-fields)
   "At point, create a BibTeX entry using information extracted
   from the HTML-BUFFER."
   (bibtex-mode)
@@ -104,22 +104,21 @@ FILE field to the entry."
      html-buffer
      '("\"_bibtex\":\"\\(@.+?}\\)\"" . 1)
      (replace-regexp-in-string "forum" "pdf" url)
-     (list
-      '("abstract" .
-	("<meta name=\"citation_abstract\" content=\"\\(.+?\\(\n.*?\\)*?\\)\"/>" . 1))
-      '("area" .
+     '("abstract" .
+       ("<meta name=\"citation_abstract\" content=\"\\(.+?\\(\n.*?\\)*?\\)\"/>" . 1))
+     '("area" .
        ("\"Please_choose_the_closest_area_that_your_submission_falls_into\":\"\\(.+?\\)\"" . 1))
-      '("keywords" . ("Keywords.*?\"note-content-value\">\\(.+?\\)</span>" . 1))
-      '("summary" .
-	("\\(Summary\\|TL;DR\\).*?\"note-content-value\">\\(.+?\\)</span>" . 2))
-      ;; Should we proactively download supplementary materials too?
-      (cons "supp"
-	    (let ((supp (org-ref--extract
-			 html-buffer
-			 ">Supplementary Material<.*?href=\"\\([^\"]+\\)" 1)))
-	      (if supp
-		  (concat "https://openreview.net" supp)
-		nil)))))))
+     '("keywords" . ("Keywords.*?\"note-content-value\">\\(.+?\\)</span>" . 1))
+     '("summary" .
+       ("\\(Summary\\|TL;DR\\).*?\"note-content-value\">\\(.+?\\)</span>" . 2))
+     ;; Should we proactively download supplementary materials too?
+     (cons "supp"
+	   (let ((supp (org-ref--extract
+			html-buffer
+			">Supplementary Material<.*?href=\"\\([^\"]+\\)" 1)))
+	     (if supp
+		 (concat "https://openreview.net" supp)
+	       nil))))))
 
 
 (defun org-ref-extract-from-pmlr (url)
@@ -137,7 +136,7 @@ FILE field to the entry."
 	 '("id=\"bibtex\">\n\\(@.+\\(\n.*?\\)+?\\)\n</" . 1)
 	 '("{\\(http.+\\.pdf\\)}" . 1)
 	 ;; Should we proactively download supplementary materials too?
-	 '(("supp" . ("href=\"\\(https?://proceedings\\.mlr\\.press/[^\"]+?-supp[^\"]*?\\)\".*?>Supplementary PDF</" . 1))))))))
+	 '("supp" . ("href=\"\\(https?://proceedings\\.mlr\\.press/[^\"]+?-supp[^\"]*?\\)\".*?>Supplementary PDF</" . 1)))))))
 
 
 (defun org-ref-extract-from-neurips (url)
@@ -161,22 +160,18 @@ FILE field to the entry."
     (org-ref--extract-entry-from-html
      html-buffer
      bibtex
-     (concat
-      neurips-url
-      (org-ref--extract html-buffer
-			"href=[\"']\\([^\"']+-Paper[^\"']*\\)[\"']" 1))
-     (list (cons "url" url)
-	   '("abstract" .
-	     ("<h4>Abstract</h4>[ \n]*?\\(<p>\\)+\\(.+?\\)</p>" . 2))
-	   ;; Should we proactively download supplementary materials too?
-	   (cons
-	    "supp"
-	    (let ((supp (org-ref--extract
-			 html-buffer
-			 "href=[\"']\\([^\"']+-Supplemental[^\"']*\\)[\"']" 1)))
-	      (if supp
-		  (concat neurips-url supp)
-		nil)))))))
+     (concat neurips-url
+	     (org-ref--extract html-buffer
+			       "href=[\"']\\([^\"']+-Paper[^\"']*\\)[\"']" 1))
+     (cons "url" url)
+     '("abstract" . ("<h4>Abstract</h4>[ \n]*?\\(<p>\\)+\\(.+?\\)</p>" . 2))
+     ;; Should we proactively download supplementary materials too?
+     (cons "supp"
+	   (if-let
+	       ((supp (org-ref--extract
+		       html-buffer
+		       "href=[\"']\\([^\"']+-Supplemental[^\"']*\\)[\"']" 1)))
+	       (concat neurips-url supp))))))
 
 
 (defun org-ref-extract-from-cvf (url)
@@ -195,14 +190,13 @@ FILE field to the entry."
      '("class=\"bibref[^\"]*\">[ \n]*\\(@.+?\\(\n.*?\\)+?\\)[ \n]*</" . 1)
      (concat cvf-url (org-ref--extract
 		      html-buffer "<a href=[\"']\\([^\"']+\\)[\"']>pdf</a>" 1))
-     (list (cons "url" url)
-	   '("abstract" . ("id=\"abstract\">[ \n]*\\([^<]+\\)[ \n]*</" . 1))
-	   ;; Should we proactively download supplementary materials too?
-	   (cons "supp"
-		 (concat cvf-url
-			 (org-ref--extract html-buffer
-					   "href=[\"']\\([^\"']+\\)[\"']>supp</"
-					   1)))))))
+     (cons "url" url)
+     '("abstract" . ("id=\"abstract\">[ \n]*\\([^<]+\\)[ \n]*</" . 1))
+     ;; Should we proactively download supplementary materials too?
+     (cons "supp" (concat cvf-url
+			  (org-ref--extract html-buffer
+					    "href=[\"']\\([^\"']+\\)[\"']>supp</"
+					    1))))))
 
 
 (provide 'org-ref-extract)
