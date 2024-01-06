7863b97d0bb6745b89675eb9dba910d1dc9cc2a9
Your Name <you@example.com>
2024-01-06 11:59:44 -0500

    Fix some issues and add features in openalex
    - update how the mailto is used in the url
    - make oa--replacer more robust
    - update some json changes from openalex
    - fix defhydra commands by adding docstrings
    - improved oa-author output
    - added fulltext-search
    - added oa-coa for co-authors in last 5 years

--- openalex.el
+++ openalex.el
@@ -41,7 +41,9 @@ https://docs.openalex.org/api-entities/works"
 			   (concat "?" filter entity-id)
 			 (concat "/" entity-id))
 		       (if user-mail-address
-			   (concat "?mailto=" user-mail-address)
+			   (if filter
+			       (concat "&mailto=" user-mail-address)
+			     (concat "?mailto=" user-mail-address))
 			 "")))
 	 (req (request url :sync t :parser 'oa--response-parser))
 	 (data (request-response-data req)))
@@ -55,7 +57,10 @@ https://docs.openalex.org/api-entities/works"
 ;; This section provides a replacer and helper function to format org-entries
 ;; from the results returned in OpenAlex.
 
-(defun oa--replacer (query object)
+
+
+
+(defun oa--replacer (key object)
   "Replacer function for `s-format'.
 QUERY is a string that is either a sexp for a function to
 evaluate or a dot notation path to data in OBJECT. If QUERY is a
@@ -63,15 +68,19 @@ sexp, it is read and evaluated. Otherwise, the path is split, and
 looked up sequentially in object.
 
 OBJECT is a plist, usually from a Work request."
-  (if (s-starts-with? "(" query)
-      ;; this is a function
-      (eval (read query))
+  (if (or (s-starts-with? "(" key)
+	  (boundp (intern-soft key)))
+      ;; this is a function we should evaluate
+      ;; it is kind of janky and might miss context.
+      (eval (read key) t)
     ;; just get data
-    (let ((fields (s-split "\\." query))
+    (let ((fields (s-split "\\." key))
 	  result)
       (while fields
 	(setq object (plist-get object (intern-soft (concat ":" (pop fields))))))
       (or (string-replace "\\n" "" (format "%s" object)) "Not found"))))
+
+
 
 
 ;; ** help functions for complex data
@@ -130,7 +139,7 @@ WORKS is a list of results from OpenAlex."
 	   collect
 	   (s-format "** ${title}
 :PROPERTIES:
-:HOST: ${host_venue.display_name}
+:HOST: ${primary_location.source.display_name}
 :YEAR: ${publication_year}
 :CITED_BY_COUNT: ${cited_by_count}
 :AUTHOR: ${(oa--authors wrk)}
@@ -204,7 +213,7 @@ elisp:org-columns    elisp:org-columns-quit
 	     (plist-get wrk :oa-url)
 	     (s-format ":PROPERTIES:
 :TITLE: ${title}
-:HOST: ${host_venue.display_name}
+:HOST: ${primary_location.source.display_name}
 :AUTHOR: ${(oa--authors wrk)}
 :DOI: ${doi}
 :YEAR: ${publication_year}
@@ -235,7 +244,7 @@ elisp:org-columns    elisp:org-columns-quit
 	     (plist-get wrk :oa-url)
 	     (s-format ":PROPERTIES:
 :TITLE: ${title}
-:HOST: ${host_venue.display_name}
+:HOST: ${primary_location.source.display_name}
 :AUTHOR: ${(oa--authors wrk)}
 :DOI: ${doi}
 :YEAR: ${publication_year}
@@ -276,7 +285,7 @@ elisp:org-columns    elisp:org-columns-quit
 	     url
 	     (s-format ":PROPERTIES:
 :TITLE: ${title}
-:HOST: ${host_venue.display_name}
+:HOST: ${primary_location.source.display_name}
 :AUTHOR: ${(oa--authors wrk)}
 :DOI: ${doi}
 :YEAR: ${publication_year}
@@ -327,7 +336,7 @@ With prefix arg ASCENDING sort from low to high."
 (defun oa-related-works ()
   "Open the side window for Related works on cite at point."
   (interactive)
-  (oa--related-works (concat "doi:" (org-ref-get-doi-at-point))))
+  (oa--related-works (concat "https://doi.org/" (org-ref-get-doi-at-point))))
 
 
 (defun oa-referenced-works ()
@@ -342,9 +351,18 @@ With prefix arg ASCENDING sort from low to high."
   (oa--cited-by-works (concat "doi:" (org-ref-get-doi-at-point))))
 
 
-(defhydra+ org-ref-citation-hydra () ("ar" oa-related-works "Related documents" :column "OpenAlex"))
-(defhydra+ org-ref-citation-hydra () ("ac" oa-cited-by-works "Cited by documents" :column "OpenAlex"))
-(defhydra+ org-ref-citation-hydra () ("af" oa-referenced-works "References from" :column "OpenAlex"))
+
+(defhydra+ org-ref-citation-hydra ()
+  "Add related documents action to `org-ref-citation-hydra'."
+  ("xr" oa-related-works "Related documents" :column "OpenAlex"))
+
+(defhydra+ org-ref-citation-hydra ()
+  "Add cited by documents action to `org-ref-citation-hydra'."
+  ("xc" oa-cited-by-works "Cited by documents" :column "OpenAlex"))
+
+(defhydra+ org-ref-citation-hydra ()
+  "Add references from action to `org-ref-citation-hydra'."
+  ("xf" oa-referenced-works "References from" :column "OpenAlex"))
 
 
 ;; * utilities
@@ -402,7 +420,7 @@ With prefix arg ASCENDING sort from low to high."
 :ID: ${id}
 :DOI: ${ids.doi}
 :YEAR: ${publication_year}
-:HOST_VENUE: ${host_venue.display_name}
+:HOST_VENUE: ${primary_location.source.display_name}
 :AUTHORS: ${(oa--authors result)}
 :CITED_BY_COUNT: ${cited_by_count}
 :END:
@@ -416,6 +434,42 @@ ${(oa--elisp-get-bibtex result)}
     " 'oa--replacer result)))))
     entries))
 
+
+;; (defun oa-author (entity-id)
+;;   "View Author for ENTITY-ID in an org-buffer."
+;;   (let* ((buf (get-buffer-create "*OpenAlex - Author*"))
+;; 	 (data (oa--author entity-id))
+;; 	 (works-count (plist-get data :works_count))
+;; 	 (works-url (plist-get data :works_api_url))
+;; 	 (works-data (request-response-data
+;; 		      (request works-url
+;; 			:sync t
+;; 			:parser 'oa--response-parser))))
+;;     (with-current-buffer buf
+;;       (erase-buffer)
+;;       (insert (s-format "* ${display_name} ([[${oa-url}][json]])
+;; :PROPERTIES:
+;; :ORCID: ${orcid}
+;; :SCOPUS: ${ids.scopus}
+;; :WORKS_COUNT: ${works_count}
+;; :CITED_BY_COUNT: ${cited_by_count}
+;; :INSTITUTION: ${last_known_institution.display_name}, ${last_known_institution.country_code}
+;; :END:
+
+;; #+COLUMNS: %25ITEM %YEAR %CITED_BY_COUNT
+;; elisp:org-columns    elisp:org-columns-quit
+
+;; #+caption: Sort
+;; | year     | [[elisp:(oa-buffer-sort-year t)][old first]] | [[elisp:(oa-buffer-sort-year)][new first]] |
+;; | cited by | [[elisp:(oa-buffer-sort-cited-by-count t)][low first]] | [[elisp:(oa-buffer-sort-cited-by-count)][high first]] |
+
+;; "
+;; 			'oa--replacer data))
+;;       (insert (s-join "\n" (oa-author-entries works-data works-url)))
+;;       (org-mode)
+;;       (goto-char (point-min))
+;;       (org-next-visible-heading 1))
+;;     (pop-to-buffer buf)))
 
 (defun oa-author (entity-id)
   "View Author for ENTITY-ID in an org-buffer."
@@ -441,6 +495,8 @@ ${(oa--elisp-get-bibtex result)}
 #+COLUMNS: %25ITEM %YEAR %CITED_BY_COUNT
 elisp:org-columns    elisp:org-columns-quit
 
+** Articles
+
 #+caption: Sort
 | year     | [[elisp:(oa-buffer-sort-year t)][old first]] | [[elisp:(oa-buffer-sort-year)][new first]] |
 | cited by | [[elisp:(oa-buffer-sort-cited-by-count t)][low first]] | [[elisp:(oa-buffer-sort-cited-by-count)][high first]] |
@@ -448,10 +504,237 @@ elisp:org-columns    elisp:org-columns-quit
 "
 			'oa--replacer data))
       (insert (s-join "\n" (oa-author-entries works-data works-url)))
+      ;; It would be nice to have a table or two with citations by year
+      (insert "\n** Bibliometrics\n\n")
+
+      (let ((data (oa--author-entries works-data works-url))
+	    (years '())
+	    (cites '()))
+	(cl-loop for result in data
+		 do
+		 (if (cdr (assoc (plist-get result :publication_year) years))
+		     (setf (cdr (assoc (plist-get result :publication_year) years))
+			   (+ 1 (cdr (assoc (plist-get result :publication_year) years))))
+		   (push (cons (plist-get result :publication_year) 1) years)))
+	(insert (format "*** Publication count by year
+
+| year | count |
+|------|-------|
+%s
+" (cl-loop for (year . count) in  (sort years (lambda (a b) (< (car a) (car b))))
+	   concat
+	   (format "| %s | %s |\n" year count))))
+
+	;;  now we cature citations by year
+	(cl-loop for result in data
+		 do
+		 (cl-loop for cnt in (plist-get result :counts_by_year)
+			  do
+			  (if (cdr (assoc (plist-get cnt :year) cites))
+			      (setf (cdr (assoc (plist-get cnt :year) cites))
+				    (+ (cdr (assoc (plist-get cnt :year) cites))
+				       (plist-get cnt :cited_by_count)))
+			    (push (cons (plist-get cnt :year)
+					(plist-get cnt :cited_by_count))
+				  cites))))
+
+	(insert (format "*** Citation count by year
+
+| year | count |
+|------|-------|
+%s
+" (cl-loop for (year . count) in  (sort cites (lambda (a b) (< (car a) (car b))))
+	   concat
+	   (format "| %s | %s |\n" year count)))))
+      
       (org-mode)
       (goto-char (point-min))
       (org-next-visible-heading 1))
     (pop-to-buffer buf)))
+
+
+;; * Full text search
+
+
+(defun oa-fulltext-search (query &optional page)
+  "Perform a fulltext search on QUERY.
+PAGE is optional, and loads that page of results. Defaults to 1."
+  (interactive (list (read-string "Query: ")
+		     (read-number "Page: ")))
+  (when (null page) (setq page 1))
+  (let* ((url (format "https://api.openalex.org/works?filter=fulltext.search:%s&page=%s&mailto=%s"
+		      (url-hexify-string query)
+		      page
+		      user-mail-address))
+	 (req (request url
+		:sync t
+		:parser #'oa--response-parser))
+	 (data (request-response-data req))
+	 (metadata (plist-get data :meta))
+	 (count (plist-get metadata :count))
+	 (per-page (plist-get metadata :per_page))
+	 (npages (+ (/ count per-page) (if (= 0 (mod count per-page)) 0 1)))
+	 (results (plist-get data :results))
+	 (next-page (format "[[elisp:(oa-fulltext-search \"%s\" %s)][Next page: %s]]"
+			    query
+			    (+ page 1)
+			    (+ page 1)))
+	 (buf (get-buffer-create "*OpenAlex Full-text search*")))
+    
+    (with-current-buffer buf
+      (erase-buffer)
+      (org-mode)
+      (insert (s-format "#+title: Full-text search: ${query}
+
+[[elisp:(oa-fulltext-search \"${query}\" ${page})]]"
+			'oa--replacer data))
+      (insert (s-format
+	       "
+${meta.count} results: Page ${meta.page} of ${(format \"%s\" npages)} ${(format \"%s\" next-page)}
+\n\n"
+	       'oa--replacer data))
+      
+      (insert
+       (cl-loop for result in results concat
+		(s-format "* ${title}
+:PROPERTIES:
+:JOURNAL: ${primary_location.source.display_name}
+:AUTHOR: ${(oa--authors result)}
+:YEAR: ${publication_year}
+:OPENALEX: ${id}
+:DOI: ${ids.doi}
+:END:
+
+${(oa--elisp-get-bibtex result)}
+
+- ${(oa--elisp-get-oa-refs result)}
+- ${(oa--elisp-get-oa-related result)}
+- ${(oa--elisp-get-oa-cited-by result)}
+
+" 'oa--replacer result)))
+
+      (insert next-page)
+      
+      (goto-char (point-min)))
+    (pop-to-buffer buf)))
+
+
+;; * NSF Collaborators and Other Affiliations
+(defun oa-coa (orcid &optional COA-file)
+  "Get a list of collaborators for the past 5 years in tab-delimited form.
+This is for Table 4 in the COA_template at
+https://www.nsf.gov/bfa/dias/policy/coa/coa_template.xlsx.
+
+ORCID is a string like 0000-0003-2625-9232.
+
+If COA-FILE is non-nil write results to that file, otherwise save
+to the clipboard. You should be able to paste the results
+directly into Excel.
+
+Results are sorted in alphaphabetical order by last name.
+
+Caveats: OpenAlex provides the name in Firstname Initial Lastname
+form. I assume this can be split on spaces, and the last word is
+the last name. That is not always correct, so some manual name
+fixing may be required.
+
+The Institutions are not always reliable. I use the most recent
+institution if an author is listed multiple times. Sometimes this
+is empty, and sometimes an author has multiple institutions
+listed.
+
+Your name will be included, you will need to delete this manually
+in the Excel sheet.
+
+This only gets the coauthors in publications known to OpenAlex.
+Recently published papers are probably missing.
+"
+  (interactive "sORCID:\nfCOA-file")
+  (let* ((url (format
+	       "https://api.openalex.org/works?filter=author.orcid:https://orcid.org/%s&email=%s"
+	       orcid
+	       user-mail-address))
+	 (req (request url :sync t :parser 'oa--response-parser))
+	 (data (request-response-data req))
+	 (meta (plist-get data :meta))
+	 (count (plist-get meta :count))
+	 (per-page (plist-get meta :per_page))
+	 (pages (/ count per-page))
+	 (current-year (string-to-number (format-time-string "%Y" (current-time))))
+	 ;; this is the first page of results
+	 (results (plist-get data :results))
+	 (current-authors '()))
+
+    ;; Now we need to accumulate the rest of the results from other pages
+    (when (> (mod count per-page) 0) (cl-incf pages))
+
+    (cl-loop for i from 2 to pages
+	     do
+	     (setq purl (concat url (format "&page=%s" i))
+		   works-data (request-response-data
+			       (request purl
+				 :sync t
+				 :parser 'oa--response-parser))
+		   results (append results (plist-get works-data :results))))
+    ;; Now results is a list of your publications. We need to iterate over each
+    ;; one, and accumulate author information
+    (cl-loop for result in results do
+	     (let ((year (plist-get result :publication_year))
+		   authors)
+	       (when (> year (- current-year 5))
+		 (cl-loop for authorship in (plist-get result :authorships) do
+			  (let* ((author (plist-get authorship :author))
+				 (name (plist-get author :display_name))
+				 (name-parts (mapcar #'capitalize (split-string name)))
+				 (name-string (concat (car (last name-parts)) ", "
+						      (string-join (butlast name-parts) " ")))
+				 
+				 (institutions (plist-get authorship :institutions)) 
+				 (institution (plist-get (car institutions) :display_name)))
+			    ;; name, institution, contact info, last-active
+			    ;; we won't have contact info from openalex.
+			    (push (list name-string institution "" year
+					(plist-get result :publication_date))
+				  current-authors))))))
+    
+    (setq current-authors (sort current-authors
+				(lambda (a b)
+				  "Sort first on name, then on year in descending order"
+				  (if (string= (nth 0 a) (nth 0 b))
+				      (> (nth 3 a) (nth 3 b))
+				    (string< (car a) (car b))))))
+    
+    ;; now filter for unique authors
+    (setq current-authors (cl-loop for group in (seq-group-by (lambda (x)
+								(car x))
+							      current-authors)	   
+   				   collect (cadr group)))
+
+    ;; Finally lets fix the year so Excel reads it correctly. I use the publication date
+    (setq current-authors (cl-loop for row in current-authors 
+				   collect
+				   (list (nth 0 row)
+					 (nth 1 row)
+					 (nth 2 row)
+					 (nth 4 row))))
+
+    (if COA-file
+	(with-temp-file COA-file
+	  (cl-loop for row in current-authors do
+		   (insert (string-join (mapcar (lambda (x)
+						  (format "%s" (or x "")))
+						row)
+					"\t")
+			   "\n")))
+      
+
+      (kill-new (mapconcat (lambda (row)
+			     (concat (string-join (mapcar (lambda (x)
+							    (format "%s" (or x "")))
+							  row)
+						  "\t")
+				     "\n"))
+			   current-authors)))))
 
 
 (provide 'openalex)
