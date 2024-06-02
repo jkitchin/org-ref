45bfe181d90482cce55be7d748bc336dd7bb90ab
John Kitchin <jkitchin@andrew.cmu.edu>
2024-06-02 09:13:59 -0400

    update copyright year

--- org-ref-utils.el
+++ org-ref-utils.el
@@ -1,6 +1,6 @@
 ;;; org-ref-utils.el --- Utility functions for org-ref  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2016-2021  John Kitchin
+;; Copyright (C) 2016-2024  John Kitchin
 
 ;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
 ;; Keywords:
