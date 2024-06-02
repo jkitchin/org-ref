9cd77c2b20dc4c4a5036c443a46d8edce6a6f2b0
John Kitchin <jkitchin@andrew.cmu.edu>
2024-06-02 09:14:16 -0400

    update copyright year

--- org-ref-wos.el
+++ org-ref-wos.el
@@ -1,6 +1,6 @@
 ;;; org-ref-wos.el --- Web of Science functions              -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015  John Kitchin
+;; Copyright (C) 2015-2024  John Kitchin
 
 ;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
 ;; Keywords:
