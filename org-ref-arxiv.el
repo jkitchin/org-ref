970879bbfb2bdeb73375f1e85e390c808fe91d52
John Kitchin <jkitchin@andrew.cmu.edu>
2024-06-02 08:52:59 -0400

    update copyright

--- org-ref-arxiv.el
+++ org-ref-arxiv.el
@@ -1,6 +1,6 @@
 ;;; org-ref-arxiv.el --- arxiv utilities for org-mode        -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2015-2021  John Kitchin
+;; Copyright (C) 2015-2024  John Kitchin
 
 ;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
 ;; Keywords:
