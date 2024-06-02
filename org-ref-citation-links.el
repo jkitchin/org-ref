2c1356874819a3b2a26595e72988623fbeebc083
John Kitchin <jkitchin@andrew.cmu.edu>
2024-06-02 08:54:00 -0400

    update copyright year

--- org-ref-citation-links.el
+++ org-ref-citation-links.el
@@ -1,6 +1,6 @@
 ;;; org-ref-citation-links.el --- citation links for org-ref -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2021  John Kitchin
+;; Copyright (C) 2024  John Kitchin
 
 ;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
 ;; Keywords: convenience
