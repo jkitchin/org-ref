a55c3750e9ef89cd094fc13fca319ba0fe252550
John Kitchin <jkitchin@andrew.cmu.edu>
2024-06-02 08:54:19 -0400

    update copyright year

--- org-ref-core.el
+++ org-ref-core.el
@@ -1,6 +1,6 @@
 ;;; org-ref-core.el --- citations, cross-references and bibliographies in org-mode -*- lexical-binding: t; -*-
 
-;; Copyright(C) 2014-2021 John Kitchin
+;; Copyright(C) 2014-2024 John Kitchin
 
 ;; This file is not currently part of GNU Emacs.
 
